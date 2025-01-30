d52e37edcd435c492a3cf49c1cbe80d0a5469b39
Danielle Mcloughlin <184102558+daniellem62@users.noreply.github.com>
2025-01-30 15:01:21 +0000

    Added components to app.jsx and started api request

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,34 @@
-import { useState } from 'react'
-import './App.css'
+import { useState, useEffect } from "react";
+import "./App.css";
+import axios from "axios";
+import Header from "./components/Header/Header.jsx";
+import Search from "./components/Search/Search.jsx";
+//import PlantCard from './components/PlantCard/PlantCard.jsx'
 
 function App() {
+  const [plants, setPlants] = useState([]);
 
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await axios.get(
+          `https://perenual.com/api/species-list?key=sk-7S5M679b6bd4db7498444`
+        );
+        setPlants(response.data[0]); // This will set only the first result
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    }
+
+    fetchData();
+  }, []);
   return (
     <>
-     
+      <Header />
+      <Search />
     </>
-  )
+  );
 }
 
-export default App
+//<PlantCard data={plants}/>
+export default App;
