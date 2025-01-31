ff66410bcc66cf75d2a73297c6691581abe9a4f8
Danielle Mcloughlin <184102558+daniellem62@users.noreply.github.com>
2025-01-31 10:47:00 +0000

    Changed styling for suggested plants

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,13 @@ import { useState } from "react";
 import "./App.css";
 import Header from "./components/Header/Header.jsx";
 import Search from "./components/Search/Search.jsx";
-import PlantCard from './components/PlantCard/PlantCard.jsx'
+import PlantCard from "./components/PlantCard/PlantCard.jsx";
 import { plantData } from "./assets/data.js";
 import Footer from "./components/Footer/Footer.jsx";
 
 import SuggestedPlants from "./components/SuggestedPlants/SuggestedPlants.jsx";
 
 import Filter from "./components/Filter/Filter.jsx";
-
-
 
 function App() {
   const [plants, setPlants] = useState(plantData); // All plants (initial data)
@@ -23,43 +21,41 @@ function App() {
     setSelectedPlant(null); // Reset the selected plant when searching
   };
 
-
   const handleSelectPlant = (plant) => {
     setSelectedPlant(plant); // Update the selected plant state
-  }
+  };
 
   // Filter plants based on searchQuery
   // const filteredPlants = plants.filter((plant) =>
   //   plant.common_name.toLowerCase().includes(searchQuery.toLowerCase())
   // );
 
-  const filteredPlants = selectedPlant
-    ? [selectedPlant]
-    : plants.filter((plant) =>
-        plant.common_name.toLowerCase().includes(searchQuery.toLowerCase())
-      );
-
   function handleFilterChange(value) {
     setFilter(value);
   }
 
   // Filter plants based on searchQuery
-  const filteredPlants = plants.filter((plant) => {
-    const matchesSearch = plant.common_name.toLowerCase().includes(searchQuery.toLowerCase());
-    const matchesFilter = filter ? plant.sunlight === filter : true;
-    return matchesSearch && matchesFilter;
-  });
-
+  const filteredPlants = selectedPlant
+    ? [selectedPlant]
+    : plants.filter((plant) => {
+        const matchesSearch = plant.common_name
+          .toLowerCase()
+          .includes(searchQuery.toLowerCase());
+        const matchesFilter = filter ? plant.sunlight === filter : true;
+        return matchesSearch && matchesFilter;
+      });
 
   return (
     <div>
       <Header />
       <Search handleSearch={handleSearch} /> {/* Correct prop name */}
-      <SuggestedPlants plants={plants} onSelectPlant={handleSelectPlant}/> {/* Pass all plants */}
-      <Filter handleFilterChange={handleFilterChange} filter={filter} /> {/* Correct prop names */}
+      <SuggestedPlants plants={plants} onSelectPlant={handleSelectPlant} />{" "}
+      {/* Pass all plants */}
+      <Filter handleFilterChange={handleFilterChange} filter={filter} />{" "}
+      {/* Correct prop names */}
       <h1>Plant Information</h1>
       <PlantCard plants={filteredPlants} /> {/* Use filteredPlants */}
-      <Footer/>
+      <Footer />
     </div>
   );
 }
